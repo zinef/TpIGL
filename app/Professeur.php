14b6769a578993d54f1041276903e9e5dd4263b6
FODIL zine-eddine <hz_fodil@esi.dz>
2019-11-20 13:00:50 +0100

    Modification sur la classe modèle Professeur

--- app/Professeur.php
+++ app/Professeur.php
@@ -11,4 +11,19 @@ class Professeur extends User
     protected $fillable = [
         'grade','specialites','diplomes',
     ];
+
+    /*les methodes de creation des relations entre les autres modèles */ 
+    public function etudiants(){
+        return $this-> hasMany("app/Etudiant");
+    }
+
+    public function modules(){
+        return $this->hasMany("app/Module");
+    }
+
+    public function groupes(){
+        return $this->hasMany("app/Groupe");
+    }
+
+    
 }
