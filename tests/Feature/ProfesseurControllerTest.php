6bcff789d6413d5a0a602f0d8e8ee9e09ff0a414
FODIL zine-eddine <hz_fodil@esi.dz>
2019-12-28 01:59:59 +0100

    code des méthodes de tests de testIndex et testAffichageInfos

--- tests/Feature/ProfesseurControllerTest.php
+++ tests/Feature/ProfesseurControllerTest.php
@@ -27,40 +27,77 @@ class ProfesseurControllerTest extends TestCase
 
     //test de la methode ajouter note
     public function testAjouterNote(){
-        //céation de la  collection pour note
-        $collection = new Collection;
-        //remplissage de la collection
-        $collection->add((object)($ci=15.0));
-        $collection->add((object)($cf=15.0));
-        $collection->add((object)($cc=15.0));
-        $collection->add((object)($moyenne=15.0));
-        $collection->add((object)($etudiant_id=12));
-
-        //création du Mock pour la note
-        $mockNote= Mockery::mock('Note');
-        $mockNote->shouldReceive('save')
-                 ->once()
-                 ->andReturn($collection);
-
-        //création  de lien entre le mock et le conteneur de laravel
-        $this->app->instance('Note',$mockNote);
-
-        //action que fait cette methode 
-        $response = $this->call('POST','/saisirnotes/{id}');
-
-        //Assertions
-        $this->assertResponseOk();
-        $this->assertViewIs('/saisirnotes');
+        
     }
 
     //test de la méthode index
     public function testIndex(){
+        //céation de la  collection pour etudiant
+        $collection = new Collection;
+        //remplissage de la collection 
+        ///Dans ce cas on gènere quelques etudiants de cette manière ["nom"=>"etudiant".$i] ou "i" une variable de controle d'une boucle
+        $i=rand(0,10);
+        while($i--){
+            $collection->add((object)["NomEtud"=>"Etudiant".$i]);
+        }
+        //création du Mock pour etudiant
+        $mockEtudiant=Mockery::mock('Etudiant');
+        $mockEtudiant->shouldReceive('where')
+                     ->once()
+                     ->andReturn($collection);
 
+        //création du lien entre le mock et le conteneur de laravel 
+        $this->app->instance('Etudiant',$mockEtudiant);
+
+        //action que fait cette méthode
+        $response=$this->call('GET','liste/1');
+        
+        //Assertions
+        $response->assertOk();
+        $response->assertViewIs('index');
     }
 
     //test de la méthode affichageInfos
     public function testAffichageInfos(){
+        //création d'un user (professeur)
+        //NOTABENE:peu importe l'id dans ce cas je veux juste tester le bout de code de l'affichage et ses vues
+        $user=User::create([
+            "identifiant"=> "usertest",
+            "name"=>  "username",
+            "surname"=>  "usersurname",
+            "email"=>  "sample@esi.dz",
+            "telephone"=>  "+213xxxxxxxxx",
+            "adresse"=>  "unknown",
+            "dateDeNaissance"=>  "xx-xx-xxxx",
+        ]);
 
+        $professeurUser=Professeur::create([
+            "specialites"=> "Informatique/Mathématique" ,
+            "grade"=> "Maitre de conf B" ,
+        ]);
+
+        //création du mock pour l'utilisateur (user en tantque professeur )
+        $mockUser=Mockery::mock('User');
+        $mockProfesseurUser=Mockery::mock('Professeur');
+        $mockUser->shouldReceive('find')
+                 ->once()
+                 ->andReturn($user);
+
+        $mockProfesseurUser->shouldReceive(find)
+                           ->once()
+                           ->andReturn($professeurUser);
+
+        //création de liens entre les mocks et le conteneur de laravel 
+        $this->app->instance('User',$mockUser);
+        $this->app->instance('Professeur',$mockProfesseurUser);
+
+        //action que fait cette methode
+        //$response= $this->call('GET','');
+        
+        //Assertions
+        //$response->assertOk();
+        //$response->assertViewIs();
+        
     }
 
     //la methode tearDown qui détruit le mock crée 
