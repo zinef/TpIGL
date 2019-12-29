ebef4e8081a45f0b0e2b7127b44b7dfe8bafa091
hamla hichem <gh_hamla@esi.dz>
2019-12-29 08:53:01 +0100

    creation de page welcome

---
+++ resources/js/components/Welcome.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import './welcome.css';
+import {BrowserRouter as Router,Link,Route} from 'react-router-dom';
+
+
+
+export default class Welcome extends Component {
+    render(){
+        return(
+        
+            
+            <div>
+                <div className="topnav">
+            
+                
+                    <div className="topnav-centered bg-secondary">
+                    <a href="" >
+                        <h1   >ESI Scolarity</h1>
+                    </a>
+                    </div>
+                    
+                    
+                    
+                
+                    <div className="topnav-right text-white">
+
+                    <Link to='/login' className="" >login</Link>
+                    {/* <Link to='/Enseignant/Note' className="" >about us</Link> */}
+                    
+                    
+
+                      
+                   
+                    </div>
+                    
+                </div>
+                    {/* <div className='centerDiv text-center text-white'> Esi Scolarity est une platform academique
+                    pour les Enseignant ,les etudiant en l'adminstrastion</div>
+                */}
+          
+                </div>
+       
+              
+
+        );
+    }
+}
+if (document.getElementById('welcome')) {
+        ReactDOM.render(<Welcome/>, document.getElementById('welcome'));
+}
+    
