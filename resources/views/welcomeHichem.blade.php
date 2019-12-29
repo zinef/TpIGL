30c6c31fa0de2f1d83e09fba4ed10cbc98b47ef7
hamla hichem <gh_hamla@esi.dz>
2019-12-29 13:51:59 +0100

    ajouter background image

---
+++ resources/views/welcomeHichem.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <link href="css/app.css" rel="stylesheet" type="text/css" >
+        <link href="js/components/welcome.css" rel="stylesheet" type="text/css" >
+    </head>
+   
+     
+
+    
+    <body class="square" id="body">
+   
+   
+        
+      <div id="myapp"></div>
+      <!-- <div id="welcome"></div> -->
+  
+     
+     
+
+     <script type="text/javascript" src="js/app.js"></script> 
+         
+     </body>
+
+   
+</html>
